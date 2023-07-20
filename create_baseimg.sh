50dd21a09bf981f8c31f38fa66fabfeb0eebb932
SfikasTeo <sfikas.teo@gmail.com>
2023-07-20 09:42:08 +0300

    Fixed sfdisk error on ubuntu22.04.2

--- create_baseimg.sh
+++ create_baseimg.sh
@@ -134,7 +134,8 @@ EOF
 create_unattended_iso
 
 # In order to SSH into the virtual machine:
-# ssh -i ../configs/imageadmin-ssh_key -p 5222 imageadmin@localhost
+# Make sure that the keys have correct permissions (chmod 600)
+# ssh -t -i ./configs/imageadmin-ssh_key -p 5222 imageadmin@localhost
 
 # Install that base image
 rm -f "output/$IMG"
@@ -144,17 +145,29 @@ set -x
 #qemu-img create -f qcow2 -o size="$IMGSIZE" "output/$IMG"
 
 # Documentation: https://www.qemu.org/docs/master/system/invocation.html
+#	-enable-kvm \																								# Enable KVM
+# -m 4096 																										# 4GB of RAM
+# -smp 4																											# 4 cores
+# -device floppy,unit=0,drive=driveA													# 
+# -drive id=driveA,file="output/$IMG",if=none,format=qcow2		# Add the virtual disk
+# -cdrom $OUTISO																							# Add the installation media
+# -boot order=d 																							# Boot from the cdrom
+# -net nic -net user,hostfwd=tcp::5222-:22,hostfwd=tcp::5280-:80 # Forward ports 22 and 80
+# -vga qxl 																										# Use qxl video card
+# -vnc :0 																										# Redirect vga display to vnc
+# -usbdevice tablet																						# Use tablet as mouse
+
+# In case of error in host forwarding ports, make sure no other process is using them.
 
 # Emulate the installation
-#qemu-system-x86_64 \
-#	-enable-kvm \																												# Enable KVM
-#	-m 4096 \																														# 4GB of RAM
-#	-smp 4 \																														# 4 cores
-#	-device floppy,unit=0,drive=driveA																	# 
-# -drive id=driveA,file="output/$IMG",if=none,format=qcow2 \					# Add the virtual disk											
-#	-cdrom $OUTISO																											# Add the installation media
-# -boot order=d \																											# Boot from the cdrom
-#	-net nic -net user,hostfwd=tcp::5222-:22,hostfwd=tcp::5280-:80 \		# Forward ports 22 and 80
-#	-vga qxl \																													# Use qxl video card
-# -vnc :0 \																														# Redirect vga display to vnc
-#	-usbdevice tablet																										# Use tablet as mouse
+# qemu-system-x86_64 \
+# -m 4096 \
+# -smp 4 \
+# -device floppy,unit=0,drive=driveA \
+# -drive id=driveA,file="output/$IMG",if=none,format=qcow2 \
+# -cdrom $OUTISO \
+# -boot order=d \
+# -net nic -net user,hostfwd=tcp::5222-:22,hostfwd=tcp::5280-:80 \
+# -vga qxl \
+# -vnc :0 \
+# -usbdevice tablet
