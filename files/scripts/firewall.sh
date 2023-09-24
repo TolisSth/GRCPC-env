317e478da937e0f4b30804cfdf1970ae2ad51db2
alexspitalas <alexspitalas@yahoo.gr>
2023-09-24 09:18:49 +0300

    adnfafnadnsaknsd ;ajsndfa

--- files/scripts/firewall.sh
+++ files/scripts/firewall.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-ufw allow from 155.207.1.94 to any port 443 proto tcp
-ufw allow from any to 155.207.1.94 port 443 proto tcp
-ufw allow to 127.0.0.1/24
-ufw allow from 127.0.0.1/24
-ufw allow out to 127.0.0.1/24
-ufw allow out from 127.0.0.1/24
+#ufw allow from 155.207.1.94 to any port 443 proto tcp
+#ufw allow from any to 155.207.1.94 port 443 proto tcp
+#ufw allow to 127.0.0.1/24
+#ufw allow from 127.0.0.1/24
+#ufw allow out to 127.0.0.1/24
+#ufw allow out from 127.0.0.1/24
+ufw allow from 155.207.131.147
+ufw allow out to 155.207.131.147 
+ufw allow from 150.140.140.3
+ufw allow out to 150.140.140.3
+ufw allow from 147.102.38.187 
+ufw allow out to 147.102.38.187
+ufw allow from 192.168.0.0/16 
+ufw allow out to 192.168.0.0/16
 ufw allow ssh
 
 
